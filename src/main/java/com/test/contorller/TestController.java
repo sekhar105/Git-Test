2d08e4f510d2463ef14185b5a4146860800e1cfa
sekhar105 <repaka99999>
2025-04-19 11:41:39 +0530

    first commit

---
+++ src/main/java/com/test/contorller/TestController.java
@@ -0,0 +1,13 @@
+package com.test.contorller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hi")
+    public String getName(){
+        return "hi sekhar ";
+    }
+}
---
+++ src/test/java/com/test/TestApplicationTests.java
@@ -0,0 +1,11 @@
+package com.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = "spring.flyway.enabled=false")
+class TestApplicationTests {
+
+
+
+}
