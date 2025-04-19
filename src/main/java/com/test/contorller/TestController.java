90fa2f267cbf950080063b30a1be151edbb74fab
sekhar105 <repaka99999@gmail.com>
2025-04-19 16:54:56 +0530

    Update TestController.java

--- src/main/java/com/test/contorller/TestController.java
+++ src/main/java/com/test/contorller/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    //get api with end point hi
+
     @GetMapping("/hi")
     public String getName(){
         return "hi sekhar ";
